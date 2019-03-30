104936d63ea03922eade9febc9b3b22fbf805a67
sergio_101 <sergio.rrd@gmail.com>
2019-03-29 22:59:51 -0400

    added ability to search for events

--- lib/bagel_tracker_web/controllers/page_controller.ex
+++ lib/bagel_tracker_web/controllers/page_controller.ex
@@ -1,7 +1,19 @@
 defmodule BagelTrackerWeb.PageController do
   use BagelTrackerWeb, :controller
+  alias BagelTracker.Router
+  alias BagelTracker.Event
 
-  def index(conn, _params) do
+  def index(conn, params) do
+    render(conn, "index.html")
+  end
+
+  def process_search(conn, params) do
+    if params["search_term"] && params["range"] do
+      events =
+        GoogleGeocodingApi.geo_location(params["search_term"])
+        |> Event.events_for_distance(String.to_integer(params["range"]) )
+      IO.inspect length(events)
+    end
     render(conn, "index.html")
   end
 end
--- lib/bagel_tracker_web/router.ex
+++ lib/bagel_tracker_web/router.ex
@@ -17,6 +17,7 @@ defmodule BagelTrackerWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    post "/process_search", PageController, :process_search
   end
 
   # Other scopes may use custom stacks.
